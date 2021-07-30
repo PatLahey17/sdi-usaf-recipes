7755ad1947fe54358f496812302911ab42830dc0
Evan Fillman <efillman@gmail.com>
2021-07-30 19:43:12 -0400

    added strech functionality but not tests

--- src/App.js
+++ src/App.js
@@ -1,117 +1,153 @@
-import React from 'react';
-import RecipeCard from './RecipeCard'
+import React from "react";
+import RecipeCard from "./RecipeCard";
 
 class App extends React.Component {
-  state = {
-    isAddRecipeFormDisplayed: false,
-    recipes: [],
-    newRecipeName: "",
-    newRecipeInstructions: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAddRecipeFormDisplayed: false,
+      recipes: [],
+      newRecipeName: "",
+      newRecipeInstructions: "",
+    };
+    //this.updatedRecipe = this.updatedRecipe.bind(this);
   }
-  
+
   handleRecipeNameChange = (event) => {
     const value = event.target.value;
-  
-    this.setState({newRecipeName: value});
-  }
+
+    this.setState({ newRecipeName: value });
+  };
 
   handleRecipeInstructionsChange = (event) => {
     const value = event.target.value;
-  
-    this.setState({newRecipeInstructions: value});
-  }
+
+    this.setState({ newRecipeInstructions: value });
+  };
 
   toggleAddRecipeForm = () => {
-    this.setState({isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed})
-  }
+    this.setState({
+      isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed,
+    });
+  };
 
   submitRecipe = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     //add new recipe object
-    const currentRecipes = this.state.recipes
-    currentRecipes.push(
-      {
+    const currentRecipes = this.state.recipes;
+    currentRecipes.push({
       name: this.state.newRecipeName,
-      instructions: this.state.newRecipeInstructions
-      }
-    )
-    this.setState({recipes: currentRecipes})
+      instructions: this.state.newRecipeInstructions,
+    });
+    this.setState({ recipes: currentRecipes });
 
     //clear old input state
-    this.setState({newRecipeName: ''});
-    this.setState({newRecipeInstructions: ''});
+    this.setState({ newRecipeName: "" });
+    this.setState({ newRecipeInstructions: "" });
+  };
 
+  updatedRecipe = (updatedRecipe, id) => {
+    const currentRecipes = this.state.recipes;
+    currentRecipes[id] = updatedRecipe;
+    this.setState({ recipes: currentRecipes });
+  };
 
-  }
-
-  render(){
+  render() {
     const addNewRecipeForm = (
- 
       <form id="recipe-form" className="" onSubmit={this.submitRecipe}>
         <div className="mb-3">
-        <label htmlFor="newRecipeName" className="form-label">Recipe name: </label>
-        <input type="text" className="form-control" name="newRecipeName" id="newRecipeName" onChange={this.handleRecipeNameChange} placeholder="write recipe name here..." value={this.state.newRecipeName} required/>
+          <label htmlFor="newRecipeName" className="form-label">
+            Recipe name:{" "}
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="newRecipeName"
+            id="newRecipeName"
+            onChange={this.handleRecipeNameChange}
+            placeholder="write recipe name here..."
+            value={this.state.newRecipeName}
+            required
+          />
         </div>
         <div className="mb-3">
-        <label htmlFor="newRecipeInstructions" className="form-label">Instructions:</label>
-        <textarea className="form-control" onChange={this.handleRecipeInstructionsChange}  name="newRecipeInstructions"
-          id="newRecipeInstructions" value={this.state.newRecipeInstructions}
-          placeholder="write recipe instructions here..." required />
-           </div>
-           <div className="mb-3">
-           <div className="d-grid gap-2">
-            <input type="submit" className="btn btn-primary btn-block"/>
+          <label htmlFor="newRecipeInstructions" className="form-label">
+            Instructions:
+          </label>
+          <textarea
+            className="form-control"
+            onChange={this.handleRecipeInstructionsChange}
+            name="newRecipeInstructions"
+            id="newRecipeInstructions"
+            value={this.state.newRecipeInstructions}
+            placeholder="write recipe instructions here..."
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <div className="d-grid gap-2">
+            <input type="submit" className="btn btn-primary btn-block" />
           </div>
-           </div>
+        </div>
       </form>
-  
-    )
+    );
 
     return (
-      <div className="App container" >
+      <div className="App container">
         <main className="row justify-content-center">
-          <section className ="col-md-6 col-sm-12">
+          <section className="col-md-6 col-sm-12">
             <header className="row">
               <div className="col text-center">
-               <h1 className="App-header">My Recipes</h1>
-               </div>
+                <h1 className="App-header">My Recipes</h1>
+              </div>
             </header>
-            <section className="row"> 
-              <div className="col">              
-                {
-                  this.state.isAddRecipeFormDisplayed
-                  ? addNewRecipeForm
-                  : <div className="d-grid gap-2"><button type="button" className="btn btn-info btn-block mb-3 " id="add-recipe" onClick={this.toggleAddRecipeForm}>Add Recipe</button></div>
-                }
+            <section className="row">
+              <div className="col">
+                {this.state.isAddRecipeFormDisplayed ? (
+                  addNewRecipeForm
+                ) : (
+                  <div className="d-grid gap-2">
+                    <button
+                      type="button"
+                      className="btn btn-info mb-3 "
+                      id="add-recipe"
+                      onClick={this.toggleAddRecipeForm}
+                    >
+                      Add Recipe
+                    </button>
+                  </div>
+                )}
               </div>
             </section>
-            <section className ="row">
-              <div className="col"> 
-              <hr></hr>
+            <section className="row">
+              <div className="col">
+                <hr></hr>
                 <header className="text-center">
-                  <h2>
-                    Current Recipe List
-                  </h2>
-                </header>           
-                {
-                this.state.recipes.length > 0 ?
+                  <h2>Current Recipe List</h2>
+                </header>
+                {this.state.recipes.length > 0 ? (
                   this.state.recipes.map((recipe, each) => {
-                      return (<div key={each} className="card">
-                        <h3 className="card-title text-center">{recipe.name}</h3>
-                        <p className="card-text">{recipe.instructions}</p>
-                        <RecipeCard recipeTitle={recipe.name} recipeInstructions={recipe.instructions}/>
-                      </div>
-                      )
-                    })                   
-                 :
-                <p>There are no recipes to list.</p>
-              }
+                    return (
+                      <RecipeCard
+                        key={`recipe-${each}`}
+                        id={each}
+                        recipeTitle={recipe.name}
+                        recipeInstructions={recipe.instructions}
+                        callBack={(updatedRecipe, id) => {
+                          this.updatedRecipe(updatedRecipe, id);
+                        }}
+                      />
+                    );
+                  })
+                ) : (
+                  <p>There are no recipes to list.</p>
+                )}
               </div>
             </section>
           </section>
         </main>
       </div>
-    )
+    );
   }
 }
 
--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,28 +1,111 @@
-import React from 'react';
+import React from "react";
 
 class RecipeCard extends React.Component {
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      recipeTitle: '',
-      recipeInstructions: ''
-    }
+      displayCard: true,
+      newRecipeName: this.props.recipeTitle,
+      newRecipeInstructions: this.props.recipeInstructions,
+    };
   }
 
+  handleRecipeNameChange = (event) => {
+    const value = event.target.value;
 
-  render(){
-    console.log(this.props)
-    return (<div>
-              <div>
-                {this.props.recipeInstructions}
-              </div>
-              <div>
-                {this.props.recipeTitle}
-              </div>
+    this.setState({ newRecipeName: value });
+  };
+
+  handleRecipeInstructionsChange = (event) => {
+    const value = event.target.value;
+
+    this.setState({ newRecipeInstructions: value });
+  };
+
+  toggleEditRecipeForm = () => {
+    this.setState({ displayCard: !this.state.displayCard });
+  };
+
+  submitRecipe = (event) => {
+    event.preventDefault();
+    //add new recipe object
+    const updatedRecipe = {
+      name: this.state.newRecipeName,
+      instructions: this.state.newRecipeInstructions,
+    };
+
+    this.props.callBack(updatedRecipe, this.props.id);
+
+    //clear old input state
+    this.toggleEditRecipeForm();
+  };
+
+  render() {
+    const displayRecipeCard = (
+      <div className="card">
+        <h3 className="card-title text-center">{this.props.recipeTitle}</h3>
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <p className="card-text">{this.props.recipeInstructions}</p>
+            <div className="d-grid gap-2 mb-3">
+              <button
+                type="button"
+                className="btn btn-info"
+                id="edit-recipe"
+                onClick={this.toggleEditRecipeForm}
+              >
+                Edit
+              </button>
             </div>
-    )
+          </div>
+        </div>
+      </div>
+    );
+    const displayRecipeEditForm = (
+      <div className="card">
+        <h3 className="card-title text-center">Edit Recipe</h3>
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <form id="recipe-form" className="" onSubmit={this.submitRecipe}>
+              <div className="mb-3">
+                <label htmlFor="newRecipeName" className="form-label">
+                  Recipe name:{" "}
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="newRecipeName"
+                  id="newRecipeName"
+                  onChange={this.handleRecipeNameChange}
+                  value={this.state.newRecipeName}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="newRecipeInstructions" className="form-label">
+                  Instructions:
+                </label>
+                <textarea
+                  className="form-control"
+                  onChange={this.handleRecipeInstructionsChange}
+                  name="newRecipeInstructions"
+                  id="newRecipeInstructions"
+                  value={this.state.newRecipeInstructions}
+                  required
+                />
+              </div>
+
+              <div className="d-grid gap-2 mb-3">
+                <input type="submit" className="btn btn-primary btn-block" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+
+    return this.state.displayCard ? displayRecipeCard : displayRecipeEditForm;
   }
 }
 
-export default RecipeCard
+export default RecipeCard;
